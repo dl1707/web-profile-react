b9467048cbc07e2b8db7e9177e9906495c90d1dc
unknown <darylluiz1707@gmail.com>
2024-09-17 12:12:05 +0530

    ReCeNt commit

--- src/components/profile.js
+++ src/components/profile.js
@@ -176,7 +176,8 @@ function Works({isW})
                {name:"Route",img:['Route App.png']},
                {name:"API",img:['API app.png']},
                {name:'Todo App',img:['Todo App.png']},
-               {name:'Bakery Management System',img:['Preview.png','Bakery Login.png','Menu.png','Food Details.png','Bill.png','Contact.png','Bakery DB.png']}];
+               {name:'Bakery Management System',img:['Preview.png','Bakery Login.png','Menu.png','Food Details.png','Bill.png','Contact.png','Bakery DB.png']},
+               {name:'Traffic Sign Detection and Recognition',img:['']}];
 
   useEffect(()=>                      //For some effects to happen
   {
