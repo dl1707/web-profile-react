ceaa648f7760f1a2d88d892545b268dfe0f4837f
unknown <darylluiz1707@gmail.com>
2025-05-01 22:08:41 +0530

    My Profile Web Updated till WorkPage.js

--- src/components/WebRoutes.js
+++ src/components/WebRoutes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import Profile from './components/profile';
+import Profile from './profile';
 import WorkPage from './WorkPage';
 
 export default function WebRoutes() {
