5e3337fbdb6127578cf5c9b7531b0f1004fb3817
unknown <darylluiz1707@gmail.com>
2025-05-02 08:20:23 +0530

    My Profile Web Updated till ProfilePage.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import WebRoutes from "./components/WebRoutes";
+import ProfilePage from "./components/ProfilePage";
 
 export default function App()
 {
-  return(<WebRoutes/>);
+  // return(<WebRoutes/>);
+  return(<ProfilePage/>);
 }
