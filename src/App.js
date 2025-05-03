ae90f9a0ff33233660faeb7eba95f63c02466f57
unknown <darylluiz1707@gmail.com>
2025-05-03 10:22:35 +0530

    Profile page test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import WebRoutes from "./components/WebRoutes";
-// import ProfilePage from "./components/ProfilePage";
+import ProfilePage from "./components/ProfilePage";
 
 export default function App()
 {
-  // return(<ProfilePage/>);
-  return(<h1>Hello this is web profile</h1>);
+  return(<ProfilePage/>);
+  // return(<h1>Hello this is web profile</h1>);
 }
