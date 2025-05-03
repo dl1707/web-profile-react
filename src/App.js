af50214b6b412965c3d7a767803886be14fcc1b7
unknown <darylluiz1707@gmail.com>
2025-05-03 10:05:43 +0530

    Modified to ul>li>div

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import WebRoutes from "./components/WebRoutes";
+import ProfilePage from "./components/ProfilePage";
 
 export default function App()
 {
-  return(<WebRoutes/>);
+  return(<ProfilePage/>);
 }
