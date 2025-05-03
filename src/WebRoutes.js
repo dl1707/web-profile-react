9565bbdfd4a285725698613365f8dd80f80f47b9
unknown <darylluiz1707@gmail.com>
2025-05-03 11:34:52 +0530

    workpage test

--- src/WebRoutes.js
+++ src/WebRoutes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import ProfilePage from './components/ProfilePage';
+// import ProfilePage from './components/ProfilePage';
 import WorkPage from './components/WorkPage';
 
 export default function WebRoutes() {
