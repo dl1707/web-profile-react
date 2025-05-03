626a8d9b757489de253da3692cf2abd53e032594
unknown <darylluiz1707@gmail.com>
2025-05-03 10:29:17 +0530

    WebRouted after test successful

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import WebRoutes from "./components/WebRoutes";
-import ProfilePage from "./components/ProfilePage";
 
 export default function App()
 {
-  return(<ProfilePage/>);
+  return(<WebRoutes/>);
   // return(<h1>Hello this is web profile</h1>);
 }
