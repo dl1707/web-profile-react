c27d8fa94e605501ccaa070eff2f470ca89bbc66
unknown <darylluiz1707@gmail.com>
2025-05-04 12:23:19 +0530

    Modified workpage

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -5,12 +5,19 @@ export default function WorkPage()
 {
   const location=useLocation();
   const {name, image}=location.state||{};
+  const Folder='./Works/';
+
   return (
     <div>
       <h1>{name}</h1>
-      <div style={{display:'flex', justifyContent:'center'}}>
-          <img src={image}alt="selected work item" style={{ height: 480, width:700}}/>
-      </div>
+      <ul style={{display:'flex',flexWrap:'wrap', justifyContent:'space-around', gap:'20px'}}>
+      {image.map((item,index)=>
+      (
+        <div key={index}>
+          <img src={Folder+item[index%image.length]} alt={index} style={{border:'1px solid black',borderRadius:'32px',padding:0}} height="520" width="320"/>
+        </div>
+      ))}
+      </ul>
       <div>
         <p>Description here...</p>
       </div>
