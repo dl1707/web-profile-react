a906b232a85f960e3c242b498010ff024bf931ca
unknown <darylluiz1707@gmail.com>
2025-05-04 11:26:25 +0530

    Changed image size to 550x550

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -302,7 +302,7 @@ function Certificates({isC})
       <div style={{display:'flex',flexDirection:'column',height:'800px',width:'800px', backgroundColor:'gray', position:'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', borderRadius:'10px', zIndex:1000}}>
         <div style={{alignSelf:'flex-end',cursor:'pointer',textAlign:'center', padding:'5px 10px', borderRadius:'50%', backgroundColor:"lightgray", marginRight:'10px'}} onClick={()=>setSelectedImage(null)}>x</div>
         <div style={{display:'flex', justifyContent:'center'}}>
-          <img src={selectedImage} alt="selected item" style={{width:700, height: 700}}/>
+          <img src={selectedImage} alt="selected item" style={{width:550, height: 550}}/>
         </div>
       </div>}
     </div>
